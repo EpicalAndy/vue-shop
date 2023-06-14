6cbcd69c7684dbf17fbfba75ddec07352ba5e054
Andy <lun9nin@inbox.ru>
2023-06-14 15:47:38 +0300

    Update cart form css

--- src/components/ShopCartOrder.vue
+++ src/components/ShopCartOrder.vue
@@ -1,10 +1,12 @@
 <template>
-  <v-card class="shop-cart-order">
+  <v-card class="shop-cart-order mx-2 mt-4">
     <v-toolbar>
       <v-toolbar-title>Заказ</v-toolbar-title>
     </v-toolbar>
       <v-card-text>Количество: {{ cart.allProductsCount }}</v-card-text>
-      <v-card-text>Стоимость: {{ cart.totalCost }} <span v-html="config.currency"></span></v-card-text>
+      <v-card-text>
+        Стоимость: {{ cart.totalCost }} <span v-html="config.currency"></span>
+      </v-card-text>
     <v-btn-group class="d-flex justify-space-between">
       <v-btn>Заказать</v-btn>
       <v-btn>Очистить</v-btn>
